9b883d057e0c815496a672fc3719ea4483c40c9d
Icaro Albar <icaro.albar@live.com>
2022-03-29 14:22:30 -0300

    recuperado sem build

--- components/header.jsx
+++ components/header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
                             <h4 className="text-amareloastro-500 font-semibold text-xl my-5 transform duration-1000">Market Data Cloud</h4>
                             <h1 className="text-cinzaastro-300 leading-none font-light text-5xl mb-10">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h1>
                             <p className="mb-12 text-justify text-amareloastro-700">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam assumenda quod culpa velit, nihil enim ipsum quam possimus, a veritatis tempora odit! Commodi, quos. Quisquam animi adipisci architecto repudiandae numquam.</p>
-                            <Link href="#" passHref><button type="button" className="inline-block px-6 py-2.5 bg-yellow-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-yellow-600 hover:shadow-lg focus:bg-yellow-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-700 active:shadow-lg transition duration-150 ease-in-out">Próximas turmas</button></Link>
+                            <Link href="#"><button type="button" class="inline-block px-6 py-2.5 bg-yellow-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-yellow-600 hover:shadow-lg focus:bg-yellow-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-700 active:shadow-lg transition duration-150 ease-in-out">Próximas turmas</button></Link>
                         </div>
 
                         <div className="invisible mx-auto text-cinzaastro-300 xl:visible xl:col-span-6">
--- components/section03.jsx
+++ components/section03.jsx
@@ -9,7 +9,7 @@ export default function Section03() {
         <>
             <div className={`h-full grid grid-cols-12 justify-items-center text-white ${styles.header}`}>
                 <div className="col-span-12 md:col-span-5 p-8 mx-auto flex items-center">
-                    <Image className="rounded-full" src="/images/LuizHota.jpg" width={400} height={417} alt="" />
+                    <Image className="rounded-full" src="/images/LuizHota.jpg" width={400} height={417} />
                 </div>
                 <div className="col-span-12 md:col-span-7 p-12 flex items-center">
                     <div>
