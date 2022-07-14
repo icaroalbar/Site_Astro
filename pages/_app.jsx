21ff1cff02e2ce457cbd7722880e113e9ccb2998
icaro_albar <icaro.albar@live.com>
2022-07-14 12:17:16 -0300

    tag google configurada

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,14 @@
 import '../styles/globals.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { fa } from '@fortawesome/free-regular-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+
+import TagManager from 'react-gtm-module'
+import { useEffect } from "react";
+
+
 config.autoAddCss = false
 
 import MainContainer from '../components/mainContainer'
@@ -12,6 +16,16 @@ import MainContainer from '../components/mainContainer'
 library.add(fab, fas)
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    const tagManagerArgs = {
+      gtmid: 'GTM-PBMF49V'
+    }
+
+    TagManager.initialize(tagManagerArgs)
+
+  },[])
+
   return  <MainContainer><Component {...pageProps} /></MainContainer>
 }
 
